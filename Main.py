f95b84218f3c718f96aa1d217ad0abed605beb7b
dadabit <74517890+dadabit@users.noreply.github.com>
2020-11-21 21:30:25 +0800

    some changes

--- Main.py
+++ Main.py
@@ -1,7 +1,12 @@
 import pygame as pg
 import sys
 
+# global variable
+gravity = 0.01
+movement = 0
+
 def get_fps():
+    fps = clock.get_fps()
     print(int(fps))
 
 
@@ -12,11 +17,24 @@ clock = pg.time.Clock()
 
 # bg-----------------------------------------------------------------------------------------------------
 bg_surface = pg.image.load('background.png').convert()
+# moon_surface--------------------------------------------------------------------------------------------
+moon_surface = pg.image.load('moon_surface.png').convert()
+# the rocket----------------------------------------------------------------------------------------------
+
+
+class Player():
+    rocket_surface = pg.image.load('Rocket.png').convert()
+    # get rect
+    rocket = rocket_surface.get_rect(center=(550, 400))
+    def __init__(self):
+        pass
+
+
 
 crashed = False
 
 
-
+player = Player()
 
 
 while not crashed:
@@ -29,8 +47,14 @@ while not crashed:
 
     pg.display.update()
     clock.tick(120)
-    window.blit(bg_surface, (0, 0))
-    fps = clock.get_fps()
+    movement += gravity
+    Player.rocket.centery += movement
+    #blit
+    window.blit(bg_surface, (0, 0))     #background
+    window.blit(Player.rocket_surface, Player.rocket)
+    window.blit(moon_surface, (0, 770))     #moon_surface
+
+
 
 
 
