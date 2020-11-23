afedc51c657c33a704bee8d009f5f5ecb6c71030
dadabit <74517890+dadabit@users.noreply.github.com>
2020-11-23 12:24:16 +0800

    some changes

--- Main.py
+++ Main.py
@@ -5,14 +5,16 @@ import random
 # global variable
 gravity = 0.1
 movement = 0
-power = 3
+power = 5
 screen_shake = 0
 sky_offset = [0, 0]
-ground_offset = [0, 770]
+
 
 def get_fps():
     fps = clock.get_fps()
     print(int(fps))
+def consolelog(out):
+    print(out)
 
 
 pg.init()
@@ -29,6 +31,7 @@ moon_surface = pg.image.load('moon_surface.png').convert()
 # sound----------------------------------------------------------------------------------------------
 enginesound = pg.mixer.Sound('enginefire.wav')
 
+
 class Player():
     rocket_surface = pg.image.load('Rocket.png').convert()
     # get rect
@@ -36,7 +39,15 @@ class Player():
 
     def __init__(self, life):
         self.life = life
-
+class Particles():
+    def __init__(self):
+        parti_list = []
+    def emit(self):
+        pass
+    def add(self):
+        pass
+    def delete_parti(self):
+        pass
 
 crashed = False
 fule_OK = True
@@ -53,7 +64,7 @@ while not crashed:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
                     movement = 0
-                    player.life -= 5
+                    player.life -= 20
                     movement -= power
                     screen_shake = 20
                     enginesound.play(0, 0, 1)
@@ -64,9 +75,10 @@ while not crashed:
     clock.tick(120)
     movement += gravity
     player.rocket.centery += movement
+    grux -= 2
 
     if player.life <= 0:
-        print('燃料耗尽')
+        consolelog('燃料耗尽')
         fule_OK = False
 
     if screen_shake > 0:
@@ -75,12 +87,13 @@ while not crashed:
     if screen_shake:
         sky_offset[0] = random.randint(0, 8) - 4
         sky_offset[1] = random.randint(0,10) - 4
-        ground_offset[0] = random.randint(0, 4) - 8
-        ground_offset[1] = random.randint(770, 775) - 2
+
+    if
 
 
     # blit
     window.blit(bg_surface, sky_offset)  # background
     window.blit(Player.rocket_surface, Player.rocket)
-    window.blit(moon_surface, ground_offset)  # moon_surface
+    window.blit(moon_surface, (grux, 750))  # moon_surface
+    window.blit(moon_surface, (grux + 1100, 750))
     #get_fps()
